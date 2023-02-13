045a1787f0b8a96789628631a6be50877deab110
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-13 10:39:17 +0300

    r

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-window.addEventListener('load', async () => {
-  if ('serviceWorker' in navigator) {
-    try {
-      const reg = await navigator.serviceWorker.register('./sw.js')
-      console.log('Service worker register success', reg)
-    } catch (e) {
-      console.log('Service worker register fail')
-    }
-  }
+window.addEventListener('load', () => {
 
-  await loadPosts()
-})
+    if ('serviceWorker' in navigator){
+
+        navigator.serviceWorker.register('./sw.js')
+            .then(registration => {
+                console.log('Service worker successfully registered', registration);
+            })
+            .catch(error => {
+                console.log('Service worker registration failed', error);
+            });
+    }
+});
--- sw.js
+++ sw.js
@@ -1,27 +1,29 @@
-const staticCacheName = 's-app-v4'
-const dynamicCacheName = 'd-app-v4'
+const staticCacheName = 'static-kurahruznama-v2';
+const dynamicCacheName = 'dynamic-kurahruznama-v2';
 
-const assetUrls = [
-    'index.html',
-    'offline.html',
-	'/css/font-awesome.min.css',
-	'/css/index.css',
-	'/css/RuznamaKurakh_end.css',
-    '/js/app.js',
-    '/js/main.js',
-	'/js/jquery.hijri.date.min.js',
-	'/js/jquery-3.6.0.min.js',
-	'/js/jquery-ui.min.js',
-	'/js/dayruznama.js',
-	'/js/script.js.js',
-	'/js/wwb18.min.js',
-    '/images/no-image.jpg'
-]
+const staticAssets = [
+	'./',
+    './index.html',
+    './offline.html',
+	'./css/font-awesome.min.css',
+	'./css/index.css',
+	'./css/RuznamaKurakh_end.css',
+    './js/app.js',
+    './js/main.js',
+	'./js/jquery.hijri.date.min.js',
+	'./js/jquery-3.6.0.min.js',
+	'./js/jquery-ui.min.js',
+	'./js/dayruznama.js',
+	'./js/script.js.js',
+	'./js/wwb18.min.js',
+    './images/no-image.jpg'
+];
 
 self.addEventListener('install', async event => {
-  const cache = await caches.open(staticCacheName)
-  await cache.addAll(assetUrls)
-})
+    const cache = await caches.open(staticCacheName);
+    await cache.addAll(staticAssets);
+    console.log('Service worker has been installed');
+});
 
 self.addEventListener('activate', async event => {
   const cacheNames = await caches.keys()
@@ -34,30 +36,29 @@ self.addEventListener('activate', async event => {
 })
 
 self.addEventListener('fetch', event => {
-  const {request} = event
+    console.log(`Trying to fetch ${event.request.url}`);
+    event.respondWith(checkCache(event.request));
+});
 
-  const url = new URL(request.url)
-  if (url.origin === location.origin) {
-    event.respondWith(cacheFirst(request))
-  } else {
-    event.respondWith(networkFirst(request))
-  }
-})
-
-
-async function cacheFirst(request) {
-  const cached = await caches.match(request)
-  return cached ?? await fetch(request)
+async function checkCache(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || checkOnline(req);
 }
 
-async function networkFirst(request) {
-  const cache = await caches.open(dynamicCacheName)
-  try {
-    const response = await fetch(request)
-    await cache.put(request, response.clone())
-    return response
-  } catch (e) {
-    const cached = await cache.match(request)
-    return cached ?? await caches.match('/offline.html')
-  }
+async function checkOnline(req) {
+    const cache = await caches.open(dynamicCacheName);
+    try {
+        const res = await fetch(req);
+        await cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        const cachedRes = await cache.match(req);
+        if (cachedRes) {
+            return cachedRes;
+        } else if (req.url.indexOf('.html') !== -1) {
+            return caches.match('./offline.html');
+        } else {
+            return caches.match('./images/no-image.jpg');
+        }
+    }
 }
