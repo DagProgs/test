18ea4ccd911d97f00f315a0983ecf045ff5b9649
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-02-13 10:47:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,21 @@
-const staticCacheName = 'static-kurahruznama-v4'
-const dynamicCacheName = 'dynamic-kurahruznama-v4'
+const staticCacheName = 'static-kurahruznama-v5'
+const dynamicCacheName = 'dynamic-kurahruznama-v5'
 
 const staticAssets = [
-	'./',
-    './index.html',
-    './offline.html',
-	'./css/font-awesome.min.css',
-	'./css/index.css',
-	'./css/RuznamaKurakh_end.css',
-    './js/app.js',
-    './js/main.js',
-	'./js/jquery.hijri.date.min.js',
-	'./js/jquery-3.6.0.min.js',
-	'./js/jquery-ui.min.js',
-	'./js/dayruznama.js',
-	'./js/script.js.js',
-	'./js/wwb18.min.js',
-    './images/no-image.jpg'
+    'index.html',
+    'offline.html',
+	'/css/font-awesome.min.css',
+	'/css/index.css',
+	'/css/RuznamaKurakh_end.css',
+    '/js/app.js',
+    '/js/main.js',
+	'/js/jquery.hijri.date.min.js',
+	'/js/jquery-3.6.0.min.js',
+	'/js/jquery-ui.min.js',
+	'/js/dayruznama.js',
+	'/js/script.js.js',
+	'/js/wwb18.min.js',
+    '/images/no-image.jpg'
 ];
 
 self.addEventListener('install', async event => {
