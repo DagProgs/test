e1de76dba1ce366499a19db7d54b557c4f09fc27
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-03-02 13:50:58 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -20,3 +20,11 @@ self.addEventListener('fetch', function (event) {
       })
   );
 });
+
+
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
