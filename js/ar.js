bf437e9a7db65f2f5dcd8a8ca94c7dc37458bc70
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-03-02 14:08:49 +0300

    d

--- js/ar.js
+++
@@ -1,9 +0,0 @@
-$('.my-date-ar').hijriDate({
-            showGregDate: false,
-            showWeekDay: false,
-            separator: '&nbsp&nbsp|&nbsp&nbsp',
-            weekDayLang: 'ru',
-            gregLang: 'ru',
-            hijriLang: 'ru',
-            correction: +0
-        });
