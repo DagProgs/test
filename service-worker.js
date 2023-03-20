5e619f845e5bf968ecc99b06840cb79c1dfaf8e1
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-03-20 08:16:32 -0700

    test

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,14 @@
-var cacheName = 'secondVersion2';
+var cacheName = 'secondVersion5';
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => cache.addAll([
-        './index.html'
+        './index.html',
+		'./js/dayruznama.js',
+		'./js/jquery-3.6.0.min.js',
+		'./js/db/ruznama_k.db',
+		'./css/style.css'
       ]))
   );
 });
