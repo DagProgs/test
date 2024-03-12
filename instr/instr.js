4ffcf48b191eb85ad0dbf5b80a4f390a3cd8c6a2
DagProgs <114062397+DagProgs@users.noreply.github.com>
2024-03-12 18:58:14 +0300

    u

--- instr/instr.js
+++ instr/instr.js
@@ -1,5 +1,5 @@
 // Проверяем user-agent на наличие iPhone
     if (navigator.userAgent.match(/iPhone/i)) {
       // Перенаправляем пользователя на страницу с инструкциями
-      window.location.href = 'instr.html';
+      window.location.href = 'instr.html?rand=1' + Math.random();
     }
