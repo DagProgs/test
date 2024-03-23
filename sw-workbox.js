b6285a7b269b812abec767c5e7f72349123a3379
DagProgs <114062397+DagProgs@users.noreply.github.com>
2024-03-23 15:16:08 +0300

    u

---
+++ sw-workbox.js
@@ -0,0 +1,137 @@
+importScripts('workbox-v4.3.0/workbox-sw.js');
+
+// SETTINGS
+
+// Path prefix to load modules locally
+workbox.setConfig({
+  modulePathPrefix: 'workbox-v4.3.0/'
+});
+
+// Turn on logging
+workbox.setConfig({
+  debug: true
+});
+
+// Updating SW lifecycle to update the app after user triggered refresh
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+// PRECACHING
+
+// We inject manifest here using "workbox-build" in workbox-build-inject.js
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "index.html",
+    "revision": "bc42d1930bf0f7fe405f4d8bf1643939"
+  },
+  {
+    "url": "css/style.css",
+    "revision": "216600c441ddbcb5c532c38ed5674e0b"
+  },
+  {
+    "url": "main.js",
+    "revision": "2676aa2c3d7a9eb04f238bf700c3c7d3"
+  },
+  {
+    "url": "js/calendar-ar.js",
+    "revision": "354b6295d66a7a9fb054ee57f3c7001a"
+  },
+  {
+    "url": "js/calendar-ru.js",
+    "revision": "a00564cbe2169a01b57b03b148cfb6fa"
+  },
+  {
+    "url": "js/clock.js",
+    "revision": "59cec707bbaf501a5fd944e9b573b62e"
+  },
+  {
+    "url": "js/jquery-3.6.0.min.js",
+    "revision": "0732e3eabbf8aa7ce7f69eedbd07dfdd"
+  },
+  {
+    "url": "js/line-timer.js",
+    "revision": "b3305349138e5e3c0fbabc094f3e8a8d"
+  },
+  {
+    "url": "js/prayer-days.js",
+    "revision": "e27f95ff90a415f2ef99cac42631553f"
+  },
+  {
+    "url": "js/prayer-times-mounths.js",
+    "revision": "7ed9d0e8bc6545156595e8fbd36cd55c"
+  },
+  {
+    "url": "js/pwacompat.min.js",
+    "revision": "038770ef3eb91f3e8a50a3916cb7cf28"
+  },
+  {
+    "url": "img/ramadan.png",
+    "revision": "68a642d1aa582d801540584db10abe19"
+  },
+  {
+    "url": "assets/icons/icon-128x128.png",
+    "revision": "25c8eb241d5e0c913da717f6007736b2"
+  },
+  {
+    "url": "assets/icons/icon-144x144.png",
+    "revision": "6e606e6871ccc1fdc7222dee1d72d42e"
+  },
+  {
+    "url": "assets/icons/icon-152x152.png",
+    "revision": "33b8202ee77c28c332a4fa3efee61d34"
+  },
+  {
+    "url": "assets/icons/icon-192x192.png",
+    "revision": "c5d401eb140c47f0d0a1b8880b5c8b49"
+  },
+  {
+    "url": "assets/icons/icon-384x384.png",
+    "revision": "47f069d621e0e363d1f0b560be4335dc"
+  },
+  {
+    "url": "assets/icons/icon-512x512.png",
+    "revision": "84f212482ada6ec3913a2a76d4b89c0d"
+  },
+  {
+    "url": "assets/icons/icon-72x72.png",
+    "revision": "9c82c0475577731db0e52b9fa62e8c05"
+  },
+  {
+    "url": "assets/icons/icon-96x96.png",
+    "revision": "9815fb3c4b57df1e8cda23d01fc66078"
+  }
+]);
+
+// RUNTIME CACHING
+
+// Google fonts
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30
+      })
+    ]
+  })
+);
+
+// API with network-first strategy
+workbox.routing.registerRoute(
+  /(http[s]?:\/\/)?([^\/\s]+\/)timeline/,
+  workbox.strategies.networkFirst()
+)
+
+// API with cache-first strategy
+workbox.routing.registerRoute(
+  /(http[s]?:\/\/)?([^\/\s]+\/)favorites/,
+  workbox.strategies.cacheFirst()
+)
+
+// OTHER EVENTS
+
+// Receive push and show a notification
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker]: Received push event', event);
+});
